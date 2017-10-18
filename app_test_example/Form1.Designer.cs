42bb8fc589653c0d10164d38ccbd05b7522cfe90
Aguirre Orozco Fabio Andres <aguirre.fabio.andres@accenture.com>
2017-10-18 11:36:24 -0500

    First Commit

---
+++ app_test_example/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace app_test_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buscarButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelArchivo = new System.Windows.Forms.Label();
+            this.labelHoja = new System.Windows.Forms.Label();
+            this.txtHoja = new System.Windows.Forms.TextBox();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buscarButton
+            // 
+            this.buscarButton.Location = new System.Drawing.Point(565, 29);
+            this.buscarButton.Name = "buscarButton";
+            this.buscarButton.Size = new System.Drawing.Size(75, 23);
+            this.buscarButton.TabIndex = 0;
+            this.buscarButton.Text = "Buscar";
+            this.buscarButton.UseVisualStyleBackColor = true;
+            this.buscarButton.Click += new System.EventHandler(this.buscarButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // labelArchivo
+            // 
+            this.labelArchivo.AutoSize = true;
+            this.labelArchivo.Location = new System.Drawing.Point(172, 38);
+            this.labelArchivo.Name = "labelArchivo";
+            this.labelArchivo.Size = new System.Drawing.Size(43, 13);
+            this.labelArchivo.TabIndex = 2;
+            this.labelArchivo.Text = "Archivo";
+            this.labelArchivo.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelHoja
+            // 
+            this.labelHoja.AutoSize = true;
+            this.labelHoja.Location = new System.Drawing.Point(360, 38);
+            this.labelHoja.Name = "labelHoja";
+            this.labelHoja.Size = new System.Drawing.Size(62, 13);
+            this.labelHoja.TabIndex = 3;
+            this.labelHoja.Text = "Hoja a Leer";
+            // 
+            // txtHoja
+            // 
+            this.txtHoja.Location = new System.Drawing.Point(428, 31);
+            this.txtHoja.Name = "txtHoja";
+            this.txtHoja.Size = new System.Drawing.Size(131, 20);
+            this.txtHoja.TabIndex = 4;
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(221, 31);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(133, 20);
+            this.txtArchivo.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(175, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(465, 274);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(622, 71);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(18, 274);
+            this.vScrollBar1.TabIndex = 7;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(175, 327);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(447, 18);
+            this.hScrollBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 503);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.txtHoja);
+            this.Controls.Add(this.labelHoja);
+            this.Controls.Add(this.labelArchivo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buscarButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buscarButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelArchivo;
+        private System.Windows.Forms.Label labelHoja;
+        private System.Windows.Forms.TextBox txtHoja;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ app_test_example/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+//using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace app_test_example
+{
+    public partial class Form1 : Form
+    {
+        private string hoja;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LLenarGrid(string archivo, string hoja)
+        {
+            //declaramos las variables         
+            OleDbConnection conexion = null;
+            DataSet dataSet = null;
+            OleDbDataAdapter dataAdapter = null;
+            string consultaHojaExcel = "Select * from [" + hoja + "$]";
+
+            //esta cadena es para archivos excel 2007 y 2010
+            string cadenaConexionArchivoExcel = "provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + archivo + "';Extended Properties=Excel 12.0;";
+
+            //para archivos de 97-2003 usar la siguiente cadena
+            //string cadenaConexionArchivoExcel = "provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + archivo + "';Extended Properties=Excel 8.0;";
+
+            //Validamos que el usuario ingrese el nombre de la hoja del archivo de excel a leer
+            if (string.IsNullOrEmpty(hoja))
+            {
+                MessageBox.Show("No hay una hoja para leer");
+            }
+            else
+            {
+                try
+                {
+                    //Si el usuario escribio el nombre de la hoja se procedera con la busqueda
+                    conexion = new OleDbConnection(cadenaConexionArchivoExcel);//creamos la conexion con la hoja de excel
+                    conexion.Open(); //abrimos la conexion
+                    dataAdapter = new OleDbDataAdapter(consultaHojaExcel, conexion); //traemos los datos de la hoja y las guardamos en un dataSdapter
+                    dataSet = new DataSet(); // creamos la instancia del objeto DataSet
+                    dataAdapter.Fill(dataSet, hoja);//llenamos el dataset
+                    dataGridView1.AllowUserToAddRows = true;
+                    dataGridView1.EditMode = DataGridViewEditMode.EditOnKeystroke;
+                    
+                    dataGridView1.DataSource = dataSet.Tables[0]; //le asignamos al DataGridView el contenido del dataSet                    
+                    //dataGridView1.AllowUserToAddRows = true;       //la ultima fila del datagridview se autoagrega
+                    dataGridView1.AllowUserToDeleteRows = true;     //la ultima fila del datagridview se autoelimina
+                    conexion.Close();//cerramos la conexion
+                }
+                catch (Exception ex)
+                {
+                    //en caso de haber una excepcion que nos mande un mensaje de error
+                    MessageBox.Show("Error, Verificar el archivo o el nombre de la hoja", ex.Message);
+                }
+            }
+        }
+
+        private void buscarButton_Click(object sender, EventArgs e)
+        {
+            //creamos un objeto OpenDialog que es un cuadro de dialogo para buscar archivos
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Archivos de Excel (*.xls;*.xlsx)|*.xls;*.xlsx"; //le indicamos el tipo de filtro en este caso que busque
+                                                                             //solo los archivos excel
+
+            dialog.Title = "Seleccione el archivo de Excel";//le damos un titulo a la ventana
+
+            dialog.FileName = string.Empty;//inicializamos con vacio el nombre del archivo
+
+            //si al seleccionar el archivo damos Ok
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //el nombre del archivo sera asignado al textbox
+                txtArchivo.Text = dialog.FileName;
+                hoja = txtHoja.Text; //la variable hoja tendra el valor del textbox donde colocamos el nombre de la hoja
+                LLenarGrid(txtArchivo.Text, hoja); //se manda a llamar al metodo
+
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; //se ajustan las
+                                                                                          //columnas al ancho del DataGridview para que no quede espacio en blanco (opcional)
+            }
+        }
+
+        private void ExportarDataGridViewExcel(DataGridView grd)
+        {
+            SaveFileDialog fichero = new SaveFileDialog();
+            fichero.Filter = "Excel (*.xls)|*.xls";
+            if (fichero.ShowDialog() == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Excel.Application aplicacion;
+                Microsoft.Office.Interop.Excel.Workbook libros_trabajo;
+                Microsoft.Office.Interop.Excel.Worksheet hoja_trabajo;
+                aplicacion = new Microsoft.Office.Interop.Excel.Application();
+                libros_trabajo = aplicacion.Workbooks.Add();
+                hoja_trabajo =
+                    (Microsoft.Office.Interop.Excel.Worksheet)libros_trabajo.Worksheets.get_Item(1);
+                //Recorremos el DataGridView rellenando la hoja de trabajo
+                for (int i = 0; i < grd.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < grd.Columns.Count; j++)
+                    {
+                        hoja_trabajo.Cells[i + 1, j + 1] = grd.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+                libros_trabajo.SaveAs(fichero.FileName,
+                    Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
+                libros_trabajo.Close(true);
+                aplicacion.Quit();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
