4bb75b78e1bf2d449f5e485817cf5c50dfbbf759
Aguirre Orozco Fabio Andres <aguirre.fabio.andres@accenture.com>
2017-10-23 11:44:02 -0500

    Fase1 ventana casi lista

--- app_test_example/Form1.Designer.cs
+++ app_test_example/Form1.Designer.cs
@@ -35,14 +35,27 @@
             this.txtHoja = new System.Windows.Forms.TextBox();
             this.txtArchivo = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.formato_busqueda = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.nombre_elemento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre_variable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo_elemento = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.componente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orden_ejecucion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.guardarButton = new System.Windows.Forms.Button();
+            this.actualizarButton = new System.Windows.Forms.Button();
+            this.cancelarButton = new System.Windows.Forms.Button();
+            this.implementarButton = new System.Windows.Forms.Button();
+            this.deviceButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buscarButton
             // 
-            this.buscarButton.Location = new System.Drawing.Point(565, 29);
+            this.buscarButton.Location = new System.Drawing.Point(582, 125);
             this.buscarButton.Name = "buscarButton";
             this.buscarButton.Size = new System.Drawing.Size(75, 23);
             this.buscarButton.TabIndex = 0;
@@ -53,7 +66,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(496, 161);
+            this.label1.Location = new System.Drawing.Point(496, 193);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 1;
@@ -61,7 +74,7 @@
             // labelArchivo
             // 
             this.labelArchivo.AutoSize = true;
-            this.labelArchivo.Location = new System.Drawing.Point(172, 38);
+            this.labelArchivo.Location = new System.Drawing.Point(189, 134);
             this.labelArchivo.Name = "labelArchivo";
             this.labelArchivo.Size = new System.Drawing.Size(43, 13);
             this.labelArchivo.TabIndex = 2;
@@ -71,7 +84,7 @@
             // labelHoja
             // 
             this.labelHoja.AutoSize = true;
-            this.labelHoja.Location = new System.Drawing.Point(360, 38);
+            this.labelHoja.Location = new System.Drawing.Point(377, 134);
             this.labelHoja.Name = "labelHoja";
             this.labelHoja.Size = new System.Drawing.Size(62, 13);
             this.labelHoja.TabIndex = 3;
@@ -79,57 +92,191 @@
             // 
             // txtHoja
             // 
-            this.txtHoja.Location = new System.Drawing.Point(428, 31);
+            this.txtHoja.Location = new System.Drawing.Point(445, 127);
             this.txtHoja.Name = "txtHoja";
             this.txtHoja.Size = new System.Drawing.Size(131, 20);
             this.txtHoja.TabIndex = 4;
+            this.txtHoja.Text = "sheet1";
             // 
             // txtArchivo
             // 
-            this.txtArchivo.Location = new System.Drawing.Point(221, 31);
+            this.txtArchivo.Location = new System.Drawing.Point(238, 127);
             this.txtArchivo.Name = "txtArchivo";
             this.txtArchivo.Size = new System.Drawing.Size(133, 20);
             this.txtArchivo.TabIndex = 5;
+            this.txtArchivo.Text = "book1.xlsx";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(175, 71);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.formato_busqueda,
+            this.nombre_elemento,
+            this.nombre_variable,
+            this.tipo_elemento,
+            this.componente,
+            this.orden_ejecucion});
+            this.dataGridView1.Location = new System.Drawing.Point(79, 163);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(465, 274);
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.Size = new System.Drawing.Size(651, 304);
             this.dataGridView1.TabIndex = 6;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // vScrollBar1
+            // formato_busqueda
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(622, 71);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(18, 274);
-            this.vScrollBar1.TabIndex = 7;
+            this.formato_busqueda.HeaderText = "formato_busqueda";
+            this.formato_busqueda.Items.AddRange(new object[] {
+            "By.id",
+            "By.className",
+            "By.xpath"});
+            this.formato_busqueda.Name = "formato_busqueda";
+            this.formato_busqueda.Width = 101;
             // 
-            // hScrollBar1
+            // nombre_elemento
             // 
-            this.hScrollBar1.Location = new System.Drawing.Point(175, 327);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(447, 18);
-            this.hScrollBar1.TabIndex = 8;
+            this.nombre_elemento.HeaderText = "nombre_elemento";
+            this.nombre_elemento.Name = "nombre_elemento";
+            this.nombre_elemento.Width = 116;
+            // 
+            // nombre_variable
+            // 
+            this.nombre_variable.HeaderText = "nombre_variable";
+            this.nombre_variable.Name = "nombre_variable";
+            this.nombre_variable.Width = 110;
+            // 
+            // tipo_elemento
+            // 
+            this.tipo_elemento.HeaderText = "tipo_elemento";
+            this.tipo_elemento.Items.AddRange(new object[] {
+            "Boton",
+            "Etiqueta",
+            "Caja de texo"});
+            this.tipo_elemento.Name = "tipo_elemento";
+            this.tipo_elemento.Width = 79;
+            // 
+            // componente
+            // 
+            this.componente.HeaderText = "componente";
+            this.componente.Name = "componente";
+            this.componente.Width = 91;
+            // 
+            // orden_ejecucion
+            // 
+            this.orden_ejecucion.HeaderText = "orden_ejecucion";
+            this.orden_ejecucion.Name = "orden_ejecucion";
+            this.orden_ejecucion.Width = 111;
+            // 
+            // guardarButton
+            // 
+            this.guardarButton.Location = new System.Drawing.Point(255, 479);
+            this.guardarButton.Name = "guardarButton";
+            this.guardarButton.Size = new System.Drawing.Size(149, 23);
+            this.guardarButton.TabIndex = 9;
+            this.guardarButton.Text = "Guardar en Nuevo archivo";
+            this.guardarButton.UseVisualStyleBackColor = true;
+            this.guardarButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // actualizarButton
+            // 
+            this.actualizarButton.Location = new System.Drawing.Point(126, 479);
+            this.actualizarButton.Name = "actualizarButton";
+            this.actualizarButton.Size = new System.Drawing.Size(75, 23);
+            this.actualizarButton.TabIndex = 10;
+            this.actualizarButton.Text = "Actualizar";
+            this.actualizarButton.UseVisualStyleBackColor = true;
+            this.actualizarButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // cancelarButton
+            // 
+            this.cancelarButton.Location = new System.Drawing.Point(597, 479);
+            this.cancelarButton.Name = "cancelarButton";
+            this.cancelarButton.Size = new System.Drawing.Size(99, 23);
+            this.cancelarButton.TabIndex = 11;
+            this.cancelarButton.Text = "Cerrar Aplicación";
+            this.cancelarButton.UseVisualStyleBackColor = true;
+            this.cancelarButton.Click += new System.EventHandler(this.cancelarButton_Click);
+            // 
+            // implementarButton
+            // 
+            this.implementarButton.Location = new System.Drawing.Point(463, 479);
+            this.implementarButton.Name = "implementarButton";
+            this.implementarButton.Size = new System.Drawing.Size(75, 23);
+            this.implementarButton.TabIndex = 26;
+            this.implementarButton.Text = "Implementar";
+            this.implementarButton.UseVisualStyleBackColor = true;
+            // 
+            // deviceButton
+            // 
+            this.deviceButton.Location = new System.Drawing.Point(129, 13);
+            this.deviceButton.Name = "deviceButton";
+            this.deviceButton.Size = new System.Drawing.Size(93, 46);
+            this.deviceButton.TabIndex = 27;
+            this.deviceButton.Text = "Buscar Dispositivos";
+            this.deviceButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Dispositivos encontrados"});
+            this.listBox1.Location = new System.Drawing.Point(239, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(133, 69);
+            this.listBox1.TabIndex = 28;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 29;
+            this.textBox1.Text = "device 1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(393, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 30;
+            this.textBox2.Text = "device 2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(393, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 20);
+            this.textBox3.TabIndex = 31;
+            this.textBox3.Text = "device 3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 503);
-            this.Controls.Add(this.hScrollBar1);
-            this.Controls.Add(this.vScrollBar1);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(902, 530);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.deviceButton);
+            this.Controls.Add(this.implementarButton);
+            this.Controls.Add(this.cancelarButton);
+            this.Controls.Add(this.actualizarButton);
+            this.Controls.Add(this.guardarButton);
             this.Controls.Add(this.txtArchivo);
             this.Controls.Add(this.txtHoja);
             this.Controls.Add(this.labelHoja);
             this.Controls.Add(this.labelArchivo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buscarButton);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -144,9 +291,22 @@
         private System.Windows.Forms.Label labelHoja;
         private System.Windows.Forms.TextBox txtHoja;
         private System.Windows.Forms.TextBox txtArchivo;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Button guardarButton;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button actualizarButton;
+        private System.Windows.Forms.Button cancelarButton;
+        private System.Windows.Forms.DataGridViewComboBoxColumn formato_busqueda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_elemento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_variable;
+        private System.Windows.Forms.DataGridViewComboBoxColumn tipo_elemento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn componente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orden_ejecucion;
+        private System.Windows.Forms.Button implementarButton;
+        private System.Windows.Forms.Button deviceButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- app_test_example/Form1.cs
+++ app_test_example/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
-//using Microsoft.Office.Interop.Excel;
 using Excel = Microsoft.Office.Interop.Excel;
+
 namespace app_test_example
 {
     public partial class Form1 : Form
@@ -51,16 +51,24 @@ namespace app_test_example
                 {
                     //Si el usuario escribio el nombre de la hoja se procedera con la busqueda
                     conexion = new OleDbConnection(cadenaConexionArchivoExcel);//creamos la conexion con la hoja de excel
-                    conexion.Open(); //abrimos la conexion
+                    conexion.Open(); //abrimos la conexion                    
                     dataAdapter = new OleDbDataAdapter(consultaHojaExcel, conexion); //traemos los datos de la hoja y las guardamos en un dataSdapter
                     dataSet = new DataSet(); // creamos la instancia del objeto DataSet
                     dataAdapter.Fill(dataSet, hoja);//llenamos el dataset
                     dataGridView1.AllowUserToAddRows = true;
+                    dataGridView1.AllowUserToDeleteRows = true;     //la ultima fila del datagridview se autoelimina
                     dataGridView1.EditMode = DataGridViewEditMode.EditOnKeystroke;
                     
                     dataGridView1.DataSource = dataSet.Tables[0]; //le asignamos al DataGridView el contenido del dataSet                    
-                    //dataGridView1.AllowUserToAddRows = true;       //la ultima fila del datagridview se autoagrega
-                    dataGridView1.AllowUserToDeleteRows = true;     //la ultima fila del datagridview se autoelimina
+                                                                  //dataGridView1.AllowUserToAddRows = true;       //la ultima fila del datagridview se autoagrega
+                    //dataGridView1.Columns.Add(dataGridView1.Columns[7]);
+                    //dataGridView1.Columns.Add(dataGridView1.Columns[7]);
+                    dataGridView1.Columns.Remove("Formato_busqueda");
+                    dataGridView1.Columns.Remove("nombre_elemento");
+                    dataGridView1.Columns.Remove("nombre_variable");
+                    dataGridView1.Columns.Remove("tipo_elemento");
+                    dataGridView1.Columns.Remove("componente");
+                    dataGridView1.Columns.Remove("orden_ejecucion");
                     conexion.Close();//cerramos la conexion
                 }
                 catch (Exception ex)
@@ -73,6 +81,8 @@ namespace app_test_example
 
         private void buscarButton_Click(object sender, EventArgs e)
         {
+
+
             //creamos un objeto OpenDialog que es un cuadro de dialogo para buscar archivos
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "Archivos de Excel (*.xls;*.xlsx)|*.xls;*.xlsx"; //le indicamos el tipo de filtro en este caso que busque
@@ -88,42 +98,176 @@ namespace app_test_example
                 //el nombre del archivo sera asignado al textbox
                 txtArchivo.Text = dialog.FileName;
                 hoja = txtHoja.Text; //la variable hoja tendra el valor del textbox donde colocamos el nombre de la hoja
-                LLenarGrid(txtArchivo.Text, hoja); //se manda a llamar al metodo
-
+                //dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                LLenarGrid(txtArchivo.Text, hoja); //se manda a llamar al metodo, en el mismo archivo que se ha abierto
+                actualizarButton.Enabled = true;
                 dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; //se ajustan las
-                                                                                          //columnas al ancho del DataGridview para que no quede espacio en blanco (opcional)
+                //columnas al ancho del DataGridview para que no quede espacio en blanco (opcional)
             }
         }
 
-        private void ExportarDataGridViewExcel(DataGridView grd)
+        public void exportaraexcel(DataGridView dataGridView1)
         {
-            SaveFileDialog fichero = new SaveFileDialog();
-            fichero.Filter = "Excel (*.xls)|*.xls";
-            if (fichero.ShowDialog() == DialogResult.OK)
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Application.Workbooks.Add(true);
+            int IndiceColumna = 0;
+            foreach (DataGridViewColumn col in dataGridView1.Columns) //Columnas
             {
-                Microsoft.Office.Interop.Excel.Application aplicacion;
-                Microsoft.Office.Interop.Excel.Workbook libros_trabajo;
-                Microsoft.Office.Interop.Excel.Worksheet hoja_trabajo;
-                aplicacion = new Microsoft.Office.Interop.Excel.Application();
-                libros_trabajo = aplicacion.Workbooks.Add();
-                hoja_trabajo =
-                    (Microsoft.Office.Interop.Excel.Worksheet)libros_trabajo.Worksheets.get_Item(1);
-                //Recorremos el DataGridView rellenando la hoja de trabajo
-                for (int i = 0; i < grd.Rows.Count - 1; i++)
+                IndiceColumna++;
+                excel.Cells[1, IndiceColumna] = col.Name;
+            }
+
+            int indiceFila = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows) //Filas
+            {
+                indiceFila++;
+                IndiceColumna = 0;
+                foreach (DataGridViewColumn col in dataGridView1.Columns)
                 {
-                    for (int j = 0; j < grd.Columns.Count; j++)
-                    {
-                        hoja_trabajo.Cells[i + 1, j + 1] = grd.Rows[i].Cells[j].Value.ToString();
-                    }
+                    IndiceColumna++;
+                    excel.Cells[indiceFila + 1, IndiceColumna] = row.Cells[col.Name].Value;
                 }
-                libros_trabajo.SaveAs(fichero.FileName,
-                    Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
-                libros_trabajo.Close(true);
-                aplicacion.Quit();
+            }
+            /*if (DataGridView.IsNullOrEmpty( dataGridView1))
+            {
+                MessageBox.Show("No hay datos para agregar");
+            }*/
+
+            //excel.GetSaveAsFilename();
+            //excel.GetSaveAsFilename("test_probe", "Excel (*.xlsx), *.xlsx", 1, "Save TEST");
+
+
+            //excel.Application.Workbooks.SaveAs();
+            
+            excel.Visible = true;
+            string path = @"C:\Users\aguirre.fabio.andres\Documents\TEST.xlsx";
+            bool request;
+            if (System.IO.File.Exists(path))
+            {
+                request = System.IO.File.Exists(path);
+                //System.out.print(request);
+                //String request2 = (request);
+                MessageBox.Show("Hoja de Datos guardada");
+            }
+            else
+            {
+                MessageBox.Show("Hoja de Datos No se ha guardado");
             }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            
+        }
+
+        private void LastColumnComboSelectionChanged(object sender, EventArgs e)
+        {
+            var currentcell = dataGridView1.CurrentCellAddress;
+            var sendingCB = sender as DataGridViewComboBoxEditingControl;
+            DataGridViewTextBoxCell cel = (DataGridViewTextBoxCell)dataGridView1.Rows[currentcell.Y].Cells[0];
+            cel.Value = sendingCB.EditingControlFormattedValue.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            exportaraexcel(dataGridView1);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            actualizaraexcel(dataGridView1);
+        }
+
+        public void actualizaraexcel(DataGridView dataGridView1)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Application.Workbooks.Open(txtArchivo.Text);
+
+            int IndiceColumna = 0;
+            foreach (DataGridViewColumn col in dataGridView1.Columns) //Columnas
+            {
+                IndiceColumna++;
+                excel.Cells[1, IndiceColumna] = col.Name;
+            }
+
+            int indiceFila = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows) //Filas
+            {
+                indiceFila++;
+                IndiceColumna = 0;
+                foreach (DataGridViewColumn col in dataGridView1.Columns)
+                {
+                    IndiceColumna++;
+                    excel.Cells[indiceFila + 1, IndiceColumna] = row.Cells[col.Name].Value;
+                }
+            }
+            //excel.GetSaveAsFilename(txtArchivo.Text);
+            //excel.Visible = true;
+            //excel.Workbooks.Add();
+            excel.Visible = true;
+            /*excel.Range["A1"].Value = "ID";
+            excel.Range["B1"].Value = "USUARIO";
+            excel.Range["C1"].Value = "DIRECCION";
+            excel.Range["D1"].Value = "TELEFONO";
+            excel.Range["E1"].Value = "EDAD";
+            excel.Range["A2"].Select();*/
+            MessageBox.Show("Hoja de Datos Actualizada");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actualizarButton.Enabled = false;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fomatoComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        public void insertarFormatoBusqueda(DataGridView dataGridView1)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            
+        }
+
+        private void formatoButton_Click(object sender, EventArgs e)
+        {
+            insertarFormatoBusqueda(dataGridView1);
+        }
+
+        private void cancelarButton_Click(object sender, EventArgs e)
+        {
+            //this.ResetText();
+
+            //MessageBox.Show("Hoja de Datos Cerrada");
+            DialogResult result = new Form1().ShowDialog();
+            if (result == DialogResult.OK) { 
+            MessageBox.Show("User clicked OK");
+            this.Close();
+            }else if (result == DialogResult.Cancel)
+                 MessageBox.Show("User clicked Cancel");
+            //DialogResult result = new Form1().ShowDialog();
+           /* MyDialog dialog = new Form1().MyDialog();
+            dialog.ShowDialog();
+            if (dialog.DialogResult.HasValue && dialog.DialogResult.Value)
+                 MessageBox.Show("User clicked OK");
+            else
+            MessageBox.Show("User clicked Cancel");*/
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
